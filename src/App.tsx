e2f2e6d52708c4e94174fd4585168933d649c54d
PadmeshSs <mrgilli1514@gmail.com>
2026-03-18 12:25:43 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='text-text container max-w-6xl mx-auto px-4 sm:px-6 md:px-8'>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
